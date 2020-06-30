4ac4c8098964cc05823f855c4ed1e440bfb32382
Ramiro Garcia <ramirogarciaurueta1005@gmail.com>
2020-06-30 10:23:33 -0500

    Docs agregado

---
+++ docs/precache-manifest.76ca0b02ec0bfcf0f4fef241f86f8f63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c135abcb9e820b4bfe8dcbc46b14f3c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05d57da3989710c60111",
+    "url": "/static/css/main.e6a98e35.chunk.css"
+  },
+  {
+    "revision": "5c9d624b26d3eb04979f",
+    "url": "/static/js/2.0095e734.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0095e734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d57da3989710c60111",
+    "url": "/static/js/main.ea3674e9.chunk.js"
+  },
+  {
+    "revision": "851e4c3c4aa894498d54",
+    "url": "/static/js/runtime-main.8ac25dc8.js"
+  }
+]);
